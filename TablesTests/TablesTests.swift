1a98643b5de857befa8cab4cf6dcba38c545288d
Colin Harris <col.w.harris@gmail.com>
2026-07-23 23:49:57 +1000

    Initial Commit

---
+++ TablesTests/TablesTests.swift
@@ -0,0 +1,19 @@
+//
+//  TablesTests.swift
+//  TablesTests
+//
+//  Created by Colin Harris on 23/7/2026.
+//
+
+import Testing
+@testable import Tables
+
+struct TablesTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
